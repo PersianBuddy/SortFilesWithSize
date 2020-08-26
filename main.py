a0f5d8f2d5bbefd9225da7d26ba7ba1bda458ae9
PersianBuddy <mnajafi0014@gmail.com>
2020-08-26 20:04:45 +0430

    Add main files and some todo list

---
+++ main.py
@@ -0,0 +1,4 @@
+# TODO: walk throgh all files in current directory
+# TODO: for each file create a dictionary that contain it's path and size
+# TODO: sort files based on theire size and print them
+# TODO: add a function that make file size more readable
